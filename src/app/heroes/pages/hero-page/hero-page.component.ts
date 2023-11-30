01a23555e3e9b08347b79cb8c80c80469182ba94
Roberto-Irigoyen-ou <roberto.irigoyen@oubita.com>
2023-11-30 13:07:20 +0100

    Pagina de vision de cada heroe

--- src/app/heroes/pages/hero-page/hero-page.component.ts
+++ src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroesService } from './../../services/heroes.service';
+import { Component, OnInit } from '@angular/core';
+import { delay, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-hero-page',
   templateUrl: './hero-page.component.html',
   styleUrls: ['./hero-page.component.scss']
 })
-export class HeroPageComponent {
+export class HeroPageComponent implements OnInit {
 
+  public hero?: Hero;
+
+  constructor(private heroesService: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router) {
+
+  }
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(
+        delay(500),
+        switchMap(({ id }) => this.heroesService.getHeroById(id))
+      ).subscribe(hero => {
+        if (!hero) return this.router.navigate(['/heroes/list']);
+        console.log({ hero });
+        return this.hero = hero;
+      })
+  }
+
+  goBack() {
+    this.router.navigateByUrl('/heroes/list')
+  }
 }
