3b01c72eadb530321c61dbabb0b0a9f797382e46
Roberto-Irigoyen-ou <roberto.irigoyen@oubita.com>
2023-12-12 14:47:53 +0100

    Public guard, impedir entrar a login si estas logueado

--- src/app/auth/guards/public.guard.ts
+++ src/app/auth/guards/public.guard.ts
@@ -1,11 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { ActivatedRouteSnapshot, CanActivate, CanMatch, Route, UrlSegment, RouterStateSnapshot, Router } from '@angular/router';
-import { Observable, tap } from 'rxjs';
+import { CanMatch, CanActivate, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable, map, tap } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({ providedIn: 'root' })
-export class AuthGuard implements CanMatch, CanActivate {
+export class PublicGuard implements CanMatch, CanActivate {
     constructor(private authService: AuthService, private router: Router) { }
 
 
@@ -16,10 +15,11 @@ export class AuthGuard implements CanMatch, CanActivate {
                     console.log('Auth: ', isAuthenticated);
                 }),
                 tap(isAuthenticated => {
-                    if (!isAuthenticated) {
-                        this.router.navigate(['./auth/login'])
+                    if (isAuthenticated) {
+                        this.router.navigate(['./'])
                     }
-                })
+                }),
+                map(isAuthenticated => !isAuthenticated)
 
             )
 
